b005b0f20d8463f3709858e134c2ecba35a37db7
fredabijay <fredabijay@gmail.com>
2022-06-15 14:38:21 +0800

    spread changed

--- src/app/spread/spread.component.ts
+++ src/app/spread/spread.component.ts
@@ -16,10 +16,6 @@ export class SpreadComponent implements OnInit {
 
   readonly asianCountries = ['Thailand', 'Iran', 'North Korea'];
   readonly africanCountries = ['Egypt', 'South Africa', 'Mali'];
-  readonly cities = [
-    {state:'oregon', cities:[{name:'portland',population:55000}, {name:'gresham',population:5000}]},
-    {state:'california', cities:[{name:'los angeles',population:55000}, {name:'sacramento',population:5000}]}
-  ]
 
   public mergeArrays(arr1:any, arr2:any):any {
     const combinedArrays = [...arr1, ...arr2]
